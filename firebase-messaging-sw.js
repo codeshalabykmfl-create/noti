794524208e13acdbacd71e8e7dca85892a7cacc8
codeshalabykmfl-create <codeshalabykmfl@gmail.com>
2026-03-11 12:58:52 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,11 +2,15 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAKF6Z9HSbF_m1L6QYUMAb6xc-20o2IYSc",
-  authDomain: "massege-5ec76.firebaseapp.com",
-  projectId: "massege-5ec76",
-  messagingSenderId: "837311654111",
-  appId: "1:837311654111:web:4bdf723a6f16ae301c616a"
+  messagingSenderId: "837311654111"
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+
+self.registration.showNotification(payload.notification.title,{
+body: payload.notification.body
+});
+
+});
